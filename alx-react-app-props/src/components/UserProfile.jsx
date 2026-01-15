7d405a182f9abd3bef3a1f231b962d09c414ef8b
Adebiyi Fuad <adebiyifuad07@gmail.com>
2026-01-15 14:14:30 +0100

    Add Changes

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -11,3 +11,16 @@ const UserProfile = (props) => {
 };
 
 export default UserProfile;
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
