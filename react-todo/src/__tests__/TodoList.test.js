d37641503a6386ca0ed9f6a670854829416f8ed4
Adebiyi Fuad <adebiyifuad07@gmail.com>
2026-02-19 12:49:30 +0100

    Add Changes

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import TodoList from "../components/TodoList";
@@ -6,7 +7,6 @@ describe("TodoList", () => {
 
   test("renders initial todos", () => {
     render(<TodoList />);
-
     expect(screen.getByText("Learn React")).toBeInTheDocument();
     expect(screen.getByText("Write Tests")).toBeInTheDocument();
     expect(screen.getByText("Build Projects")).toBeInTheDocument();
@@ -14,31 +14,23 @@ describe("TodoList", () => {
 
   test("adds a todo", () => {
     render(<TodoList />);
-
-    const input = screen.getByRole("textbox");
-    const button = screen.getByRole("button", { name: /add/i });
-
+    const input = screen.getByTestId("todo-input");
     fireEvent.change(input, { target: { value: "New Task" } });
-    fireEvent.click(button);
-
+    fireEvent.click(screen.getByText("Add"));
     expect(screen.getByText("New Task")).toBeInTheDocument();
   });
 
   test("toggles a todo", () => {
     render(<TodoList />);
-
     const todoItem = screen.getByText("Learn React");
     fireEvent.click(todoItem);
-
     expect(todoItem).toHaveStyle("text-decoration: line-through");
   });
 
   test("deletes a todo", () => {
     render(<TodoList />);
-
-    const deleteButtons = screen.getAllByRole("button", { name: /delete/i });
+    const deleteButtons = screen.getAllByText("Delete");
     fireEvent.click(deleteButtons[0]);
-
     expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
   });
 
